7e95a9fb344019a7bbc332f41920cc9221f02769
Tai Nguyen <tai.nguyen@mgisolutions.com>
2023-08-14 09:10:57 +0700

    first commit

---
+++ src/components/SvelteComponent.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+  import TextField from "./TextField.svelte";
+
+  let inputValue: string = "";
+</script>
+
+<div>
+  <div>{inputValue}</div>
+  <TextField id="test" value={inputValue} />
+</div>
---
+++ src/components/TextField.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+  export let value: string = "";
+  export let id: string = "";
+</script>
+
+<div class="text-field">
+  <label for={id} />
+  <input {id} type="text" bind:value />
+</div>
+
+<style scoped lang="scss">
+  .text-field {
+    @apply border;
+    input {
+      border: none;
+      outline: none;
+    }
+  }
+</style>
